ec910a5cc8d87211fc4fc3970b72c654ab44f064
Oguzhan <turanoguzhan58@gmail.com>
2022-11-03 18:41:30 +0300

    last commit

---
+++ DaireAlanCevreHesaplama.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class DaireAlanCevreHesaplama {
+    public static void main(String[] args) {
+        
+    double   r, delta, alan, pi=3.14;
+     
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Yarıçap(r) değerini giriniz : ");
+        r = scan.nextInt();
+
+
+        System.out.println("Merkez açının ölçüsünü giriniz : ");
+        delta = scan.nextInt();
+
+       alan = ((pi*(r*r)*delta)/360);
+       
+       System.out.println("Dairenin alanı : "+ alan);
+       
+
+
+
+
+    }
+}
