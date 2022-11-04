a8ec5f338598801dc520d86e5f0821fcbd56da10
Oguzhan <turanoguzhan58@gmail.com>
2022-11-04 13:00:54 +0300

    last commit

---
+++ VucutKitleEndeksi.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class VucutKitleEndeksi {
+    public static void main(String[] args) {
+
+        float boy,vucutKitleEndeksi;
+        int kilo;
+
+        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Lütfen boyunuzu (metre cinsinde) giriniz : ");
+        boy = scan.nextFloat();
+
+        System.out.print("Lütfen kilonuzu giriniz : ");
+        kilo = scan.nextInt();
+
+        vucutKitleEndeksi = kilo/(boy*boy);
+
+
+        System.out.println("Vücut Kitle İndeksiniz : "+vucutKitleEndeksi);
+
+
+    }
+}
