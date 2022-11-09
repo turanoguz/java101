fd9099f3f1dcb9e81af57f6261022b6d02d0cc0c
Oguzhan <turanoguzhan58@gmail.com>
2022-11-09 14:09:29 +0300

    last commit

---
+++ KullanıcıGirisi/KullanıcıGirisi.java
@@ -0,0 +1,49 @@
+package KullanıcıGirisi;
+
+import java.util.Scanner;
+
+public class KullanıcıGirisi {
+    public static void main(String[] args) {
+        
+        String userName = "patika", password = "12345", answer, inputUserName, inputPassword, newPassword;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Kullanıcı adınızı giriniz : ");
+        inputUserName = scanner.nextLine();
+
+        System.out.print("Şifrenizi giriniz : ");
+        inputPassword = scanner.nextLine();
+
+
+        if(inputUserName.equals(userName) && password.equals(password)){
+
+            System.out.println("Giriş yaptınız.");
+        }else{
+            System.out.println("Şifrenizi yanlış girdiniz. Şifrenizi sıfırlamak istermisiniz ? evet(e) veya hayır(h) ");
+            answer = scanner.nextLine();
+            if(answer.equals("e")){
+                System.out.print("Lütfen yeni şifrenizi giriniz : ");
+                newPassword = scanner.nextLine();
+
+                if(newPassword.equals(password) || newPassword.equals(inputPassword)){
+
+                    System.out.println("Şifre oluşturulamadı, lütfen başka şifre giriniz.");
+
+                }else{
+                    
+                    password = newPassword;
+                    System.out.println("Şifre Oluşturuldu.");
+
+                }
+
+
+            }
+        }
+        
+
+        
+
+    }
+    
+}
